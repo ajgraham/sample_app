accc69e6fe0e194fe30cd6f97456ecd383db9442
Alex Graham <ajgraham@syr.edu>
2011-02-02 15:52:51 -0500

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -4,12 +4,12 @@ describe "LayoutLinks" do
 
   it "should have a Home page at '/'" do
     get '/'
-    resopnse.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => "Home")
   end
   
   it "should have a Contact page at '/contact'" do
     get '/contact'
-    response.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => "Contact")
   end
 
   it "should have an About page at '/about'" do
@@ -21,5 +21,11 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  
+   it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+  
 
 end
